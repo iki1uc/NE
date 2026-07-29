7202d89d5c7c0899e544e0df652edd3df43a09cc
iki1uc <entertrainer@web.de>
2026-07-29 12:34:10 +0200

    Add NE_VERIFY function for verification process

---
+++ Verify.js
@@ -0,0 +1,11 @@
+export function NE_VERIFY(da, ben, ki) {
+    return {
+        id: "NE",
+        verify: true,
+        da,
+        ben,
+        ki,
+        state: "NE-VERIFY-OK",
+        info: "NE vergleicht DA/BEN/KI und erkennt Abweichungen"
+    };
+}
