0ea1bb5ca74cec443d46923ca0b08a42d250a720
iki1uc <entertrainer@web.de>
2026-07-29 12:33:03 +0200

    Add NE_PREFETCH function to fetch temporary data

---
+++ Prefetch.js
@@ -0,0 +1,33 @@
+export async function NE_PREFETCH() {
+
+    // ALT
+    const tmpNE  = await fetch(`NE.tmp`).then(r => r.text());
+    const tmpBEN = await fetch(`BEN.tmp`).then(r => r.text());
+    const tmpDA  = await fetch(`DA.tmp`).then(r => r.text());
+
+    // ARG / SCHACH / OPERATOR
+    const argScan   = await fetch(`RESPO_ARG_SCAN.tmp`).then(r => r.text());
+    const arg       = await fetch(`arg.tmp`).then(r => r.text());
+    const schach    = await fetch(`SCHACH.tmp`).then(r => r.text());
+    const operator  = await fetch(`operator.tmp`).then(r => r.text());
+    const op9       = await fetch(`9Operator.me`).then(r => r.text());
+
+    // ROOM
+    const room3 = await fetch(`NENC3.room`).then(r => r.text());
+    const room6 = await fetch(`NENC6e.room`).then(r => r.text());
+    const room9 = await fetch(`NENC.room`).then(r => r.text());
+
+    // SCORE
+    const score = await fetch(`score.tmp`).then(r => r.text());
+
+    return {
+        axiome: NE_AXIOM,
+        alt: { tmpNE, tmpBEN, tmpDA },
+        arg: { argScan, arg },
+        schach: { schach, op9 },
+        operator,
+        room: { room3, room6, room9 },
+        score,
+        state: "NE-PREFETCH-READY"
+    };
+}
