62247361efd61b5ebd89c627116e7714c34f6be7
iki1uc <entertrainer@web.de>
2026-07-29 12:38:54 +0200

    Rename NE_SCORE to NE_ECO and update logic

--- ECO.js
+++ ECO.js
@@ -1,19 +1,17 @@
-export function NE_SCORE(pre) {
-
-    const ARG = pre.arg.argScan.length % 100;
-    const SCHACH = pre.schach.schach.length % 100;
-    const BEN = pre.alt.tmpBEN.length % 100;
-    const NE = pre.alt.tmpNE.length % 100;
-    const ROOM = pre.room.room9.length % 100;
+export function NE_ECO(pre) {
+    const ARG = pre.argScan.length % 100;
+    const SCHACH = pre.schach.length % 100;
+    const BEN = pre.tmpBEN.length % 100;
+    const NE = pre.tmpNE.length % 100;
+    const ROOM = pre.room9.length % 100;
     const OP = pre.operator.length % 100;
 
-    const eco = 
+    return Math.round(
         0.20 * ARG +
         0.20 * SCHACH +
         0.20 * BEN +
         0.10 * (100 - NE) +
         0.20 * ROOM +
-        0.10 * OP;
-
-    return Math.round(eco);
+        0.10 * OP
+    );
 }
